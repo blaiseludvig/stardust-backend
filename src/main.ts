c3eec5e0131bafd467cb82f39e9704b28f40b370
blaiseludvig <blaiseludvig@gmail.com>
2023-03-17 07:23:52 +0100

    WIP: setting up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,15 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   app.enableCors();
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Stardust API')
+    .setDescription('Stardust API specification')
+    .setVersion('0.1')
+    .build();
+
+  const openApiDocument = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('/docs/api', app, openApiDocument);
 
   await app.listen(3000);
 }

fdc0356764d08732e9e121267efd792b0ad4182b
blaiseludvig <blaiseludvig@gmail.com>
2023-05-03 18:52:33 +0200

    remove unneccessary userId property from UpdateUserDto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsDefined } from 'class-validator';
 import CreateUserDto from './create-user.dto';
 
-export default class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsDefined()
-  userId!: number;
-}
+export default class UpdateUserDto extends PartialType(CreateUserDto) {}

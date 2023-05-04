04fe96cad85443fe619c6ec8ceb8b33b60907323
blaiseludvig <blaiseludvig@gmail.com>
2023-05-04 15:01:26 +0200

    add definition to CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDefined, IsEmail } from 'class-validator';
 
 export default class CreateUserDto {
   @IsDefined()
   @IsEmail()
+  @ApiProperty({
+    description: "The user's email address",
+    example: 'example@gmail.com',
+  })
   email!: string;
 
   @IsDefined()
+  @ApiProperty({
+    description: "The user's email password",
+    example: 'my_strong_and_secure_password',
+  })
   password!: string;
 }

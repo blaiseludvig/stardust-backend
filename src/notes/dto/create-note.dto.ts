7865988a523bbf79a2c179e5352dc44e3f8ff40b
blaiseludvig <blaiseludvig@gmail.com>
2023-05-04 15:02:30 +0200

    update the definition of CreateNoteDto

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -4,22 +4,22 @@ import { IsOptional } from 'class-validator';
 export class CreateNoteDto {
   @IsOptional()
   @ApiPropertyOptional({
-    description:
-      'The title of the note' +
-      '<br>' +
-      'Note: at least one of these properties must be defined.',
+    description: 'The title of the note',
+    example: "Hello, I'm the new note's title",
   })
   title?: string;
 
   @IsOptional()
   @ApiPropertyOptional({
     description: 'The type of the note (currently not implemented)',
+    example: 'text',
   })
   type?: string;
 
   @IsOptional()
   @ApiPropertyOptional({
     description: 'The text content of the note',
+    example: "The note's content goes here",
   })
   content?: string;
 }

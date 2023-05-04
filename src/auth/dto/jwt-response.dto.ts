907a296c4dd887978528234afbc9bd31fac67dca
blaiseludvig <blaiseludvig@gmail.com>
2023-05-04 14:58:15 +0200

    add definition to jwtResponseDto

--- src/auth/dto/jwt-response.dto.ts
+++ src/auth/dto/jwt-response.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export default class jwtResponseDto {
+  @ApiProperty({
+    description: 'The JWT token',
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  })
   access_token!: string;
 }

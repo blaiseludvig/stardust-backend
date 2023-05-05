3c31413a247e43ea2345cf82689b302016996bbb
blaiseludvig <blaiseludvig@gmail.com>
2023-05-05 17:28:16 +0200

    make class-validator stop at first error

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // enable transform for using default values in DTOs
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, stopAtFirstError: true }),
+  );
 
   app.enableCors({
     maxAge: 7200,
